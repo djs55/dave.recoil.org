89267e6a97fd94c0ea6dab6babab6aef3cacf4a6
David Scott <dave@recoil.org>
2024-08-24 20:09:56 +0100

    delete dead code
    Signed-off-by: David Scott <dave@recoil.org>

--- scripts/travis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo This travis job is intentionally left blank.
--- scripts/upload-static.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Upload the static files to a regular webserver (instead od building
-# a unikernel)
-
-rsync -avrz --delete htdocs/ dave.recoil.org:/data/www/dave.recoil.org/
-
